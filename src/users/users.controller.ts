11a777bea0c3ee1c284fb63279677bf536fadfa8
Rahul Gayen <83637505+rahulgayen@users.noreply.github.com>
2023-04-23 21:48:21 +0530

    signup-signin completed

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,25 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, NotFoundException, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
 import { Serialize, SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserDto } from './dto/user.dto';
 import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-  constructor(private userService: UsersService) {}
+  constructor(private authService: AuthService, private userService: UsersService) {}
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     const { email, password } = body;
-    return this.userService.createUser(email, password);
+    return this.authService.signup(email, password);
+  }
+  @Post('/signin')
+  @HttpCode(200)
+  signin(@Body() body: CreateUserDto) {
+    const { email, password } = body;
+    return this.authService.signin(email, password);
   }
 
   @Get(':id')
